1c4d34261a72c006fcd8426efac3779f13cca2e5
kenzkenz <kenotiai@gmail.com>
2024-10-02 23:02:07 +0900

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+<!--      <HelloWorld/>-->
+      <MyMap/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MyMap from "@/components/MyMap";
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    MyMap
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyMap.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+   aaa
+    <v-btn @click="btnClick">Button</v-btn>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'MyMap',
+  data: () => ({
+    test: 'test'
+  }),
+  methods: {
+    btnClick () {
+      console.log(this.test)
+      alert(this.$store.state.storeTest)
+    }
+  }
+}
+</script>
