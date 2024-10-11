c6f10daab4f6b8f2971de1aee6f224783a3b2304
kenzkenz <kenotiai@gmail.com>
2024-10-12 01:50:05 +0900

    no message

--- src/js/layers.js
+++ src/js/layers.js
@@ -209,8 +209,8 @@ export const amxLayerDaihyou = {
 export const bakumatsuSource = {
     id: "bakumatsu", obj: {
         type: "vector",
-        minzoom: 2,
-        maxzoom: 16,
+        minzoom: 0,
+        maxzoom: 14,
         url: "pmtiles://https://kenzkenz3.xsrv.jp/pmtiles/bakumatsu/b3.pmtiles",
         attribution:
             "<a href='' target='_blank'></a>",
