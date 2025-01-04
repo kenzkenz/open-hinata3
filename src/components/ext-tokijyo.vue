2a13d727d98e0e71c84079ae2f21090c70bd1bd8
kenzkenz <kenotiai@gmail.com>
2025-01-04 13:13:45 +0900

    no message

--- src/components/ext-tokijyo.vue
+++ src/components/ext-tokijyo.vue
@@ -201,6 +201,58 @@ export default {
       const map = this.$store.state[this.mapName]
       gistUpload(map,'oh-amx-a-fude','amx-a-pmtiles',['市区町村コード','大字コード','丁目コード','小字コード','予備コード','地番'])
     },
+    change () {
+      console.log(this.item)
+      // const vm = this
+      const map = this.$store.state[this.mapName]
+      // if (!this.s_isPaintCity) {
+      //   map.setPaintProperty(this.item.id, 'fill-color', 'rgba(0, 0, 0, 0)')
+      //   map.setPaintProperty(this.item.id + '-line', 'line-color', 'black')
+      //   map.setPaintProperty(this.item.id + '-line', 'line-width',  [
+      //     'interpolate',
+      //     ['linear'],
+      //     ['zoom'],
+      //     7, 1,
+      //     11, 4
+      //   ]);
+      // } else {
+      //   map.setPaintProperty(this.item.id, 'fill-color', ['get', 'random_color'])
+      //   map.setPaintProperty(this.item.id + '-line', 'line-color', 'black')
+      //   map.setPaintProperty(this.item.id + '-line', 'line-width', [
+      //     'interpolate',
+      //     ['linear'],
+      //     ['zoom'],
+      //     7, 0,
+      //     11, 0.5
+      //   ]);
+      // }
+      //-------------------------------------------------------------------------
+      function filterBy(text) {
+        if (text) {
+          let searchString = text
+          searchString = searchString.replace(/\u3000/g,' ').trim()
+          const words = searchString.split(" ")
+          // 複数フィールドを結合する
+         const combinedFields = ["concat", ["get", "大字名"], " ", ["get", "大字コード"], " ", ["get", "地番"], " ", ["get", "丁目コード"],
+           " ", ["get", "小字コード"], " ", ["get", "市区町村名"]];
+          // 各単語に対して、結合したフィールドに対する index-of チェックを実行
+          const filterConditions = words.map(word => [">=", ["index-of", word, combinedFields], 0]);
+          // いずれかの単語が含まれる場合の条件を作成 (OR条件)
+          const matchCondition = ["any", ...filterConditions]
+          map.setFilter('oh-amx-a-fude', matchCondition)
+          map.setFilter('oh-amx-a-fude-line', matchCondition)
+          map.setFilter('oh-amx-label', matchCondition)
+          // map.setFilter('oh-amx-a-daihyo', matchCondition)
+        } else {
+          map.setFilter('oh-amx-a-fude', null)
+          map.setFilter('oh-amx-a-fude-line', null)
+          map.setFilter('oh-amx-label', null)
+          // map.setFilter('oh-amx-a-daihyo', null)
+        }
+      }
+      filterBy(this.s_tokijyoText)
+      this.update()
+    },
   },
   mounted() {
     const map = this.$store.state[this.mapName]
@@ -212,6 +264,7 @@ export default {
   },
   watch: {
     s_extFire () {
+      this.change()
     },
   }
 }
