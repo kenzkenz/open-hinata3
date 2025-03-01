cec9a38545fdd68163c2ca6eda479307bcb39816
kenzkenz <kenotiai@gmail.com>
2025-03-02 05:31:09 +0900

    no message

--- src/php/vps/iko.php
+++ src/php/vps/iko.php
@@ -1,12 +1,10 @@
 <?php
 
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-function listDirectories($baseDir, $maxDepth = 2, $currentDepth = 0)
-{
+function listDirectories($baseDir, $maxDepth = 2, $currentDepth = 0) {
     if (!is_dir($baseDir)) {
         return [];
     }
@@ -23,7 +21,17 @@ function listDirectories($baseDir, $maxDepth = 2, $currentDepth = 0)
 
         if (is_dir($fullPath)) {
             if ($currentDepth + 1 == $maxDepth) {
-                $directories[] = $fullPath;
+                $layerJsonPath = $fullPath . '/layer.json';
+                if (file_exists($layerJsonPath)) {
+                    $jsonContent = json_decode(file_get_contents($layerJsonPath), true);
+                    if ($jsonContent && isset($jsonContent['fileName']) && isset($jsonContent['bounds'])) {
+                        $directories[] = [
+                            "path" => $fullPath,
+                            "fileName" => $jsonContent['fileName'],
+                            "bounds" => $jsonContent['bounds']
+                        ];
+                    }
+                }
             } else {
                 $directories = array_merge($directories, listDirectories($fullPath, $maxDepth, $currentDepth + 1));
             }
@@ -40,6 +48,8 @@ echo json_encode([
     "success" => true,
     "directories" => $directories
 ]);
+
+
 
 
 //
