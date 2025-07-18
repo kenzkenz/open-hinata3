734701835ae49668365da3d618221eba2323ed83
kenzkenz <kenotiai@gmail.com>
2025-07-18 12:50:05 +0900

    no message

--- src/components/Dialog-menu.vue
+++ src/components/Dialog-menu.vue
@@ -311,7 +311,7 @@ import { user as user1 } from "@/authState"; // グローバルの認証情報�
       </v-dialog>
 
       <p style="margin-top: 3px;margin-bottom: 10px;">
-        v1.230
+        v1.231
       </p>
 
       <div v-if="user1">
