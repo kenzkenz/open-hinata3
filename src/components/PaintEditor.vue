4b6ddc02b2f43f192c7d9e4fcf80e599ee18d395
kenzkenz <kenotiai@gmail.com>
2025-07-27 16:20:08 +0900

    no message

--- src/components/PaintEditor.vue
+++ src/components/PaintEditor.vue
@@ -122,7 +122,7 @@ export default {
       return this.$store.state.pmtilesLabel
     },
     s_propnames () {
-      return this.$store.state.propnames
+      return this.$store.state.propnames || []
     },
   },
   methods: {
