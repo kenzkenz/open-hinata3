765cd9544138673b32a63b2042ebb33e23aff4bc
kenzkenz <kenotiai@gmail.com>
2025-09-08 08:13:13 +0900

    no message

--- src/store/index.js
+++ src/store/index.js
@@ -47,7 +47,7 @@ const messageDialogModule = {
 export default createStore({
   state: {
     drawFeatureId: '',
-    clientVersion: 1.452,
+    clientVersion: 1.453,
     hitText: '',
     iframeVDIalog: false,
     isDrawFit: false,
