0efd72e4e7f641b1b17a6e5a4c5c4902ab1df868
kenzkenz <kenotiai@gmail.com>
2025-10-13 13:18:46 +0900

    no message

--- src/store/index.js
+++ src/store/index.js
@@ -110,7 +110,7 @@ const messageDialogModule = {
 export default createStore({
   state: {
     drawFeatureId: '',
-    clientVersion: 1.705,
+    clientVersion: 1.706,
     disabledForSokui: true,
     printMap: 'map01',
     isContextMenu: true,
